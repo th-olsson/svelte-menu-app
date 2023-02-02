57bee3d3babb01796e0ad416ff526a82fb8acf0d
Thomas Olsson <olss.thom@gmail.com>
2023-02-02 09:20:49 +0100

    adds a basic html-menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,63 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Vår meny</h1>
+<h2>Maträtter</h2>
+<table>
+	<tr>
+		<th>Maträtt</th>
+		<th>Pris</th>
+	</tr>
+	<tr>
+		<td>Lasagne</td>
+		<td>99 kr</td>
+		<td>
+			<button>-</button>
+			<button>+</button>
+		</td>
+	</tr>
+	<tr>
+		<td>Pasta</td>
+		<td>79 kr</td>
+		<td>
+			<button>-</button>
+			<button>+</button>
+		</td>
+	</tr>
+	<tr>
+		<td>Pizza</td>
+		<td>89 kr</td>
+		<td>
+			<button>-</button>
+			<button>+</button>
+		</td>
+	</tr>
+</table>
+<h2>Drycker</h2>
+<table>
+	<tr>
+		<th>Dryck</th>
+		<th>Pris</th>
+	</tr>
+	<tr>
+		<td>Vatten</td>
+		<td>19 kr</td>
+		<td>
+			<button>-</button>
+			<button>+</button>
+		</td>
+	</tr>
+	<tr>
+		<td>Cola</td>
+		<td>29 kr</td>
+		<td>
+			<button>-</button>
+			<button>+</button>
+		</td>
+	</tr>
+	<tr>
+		<td>Öl</td>
+		<td>39 kr</td>
+		<td>
+			<button>-</button>
+			<button>+</button>
+		</td>
+	</tr>
+</table>
